beaac35b80c8512482fc12c93a7de903096ca592
flavienChamay <flavien.chamay@protonmail.com>
2021-05-21 15:12:48 +0200

    visualization of temperatures

---
+++ sitka_highs.py
@@ -0,0 +1,39 @@
+"""
+Parse CSV file headers.
+
+:var filename str: The path of the CSV file to open.
+:var reader Reader: The pointer to a line of the file.
+:var reader_row Reader: The next row of the file that it reads.
+"""
+
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+filename = 'data/sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Get dates and high temperatures from the file.
+    highs, dates = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        dates.append(current_date)
+        highs.append(high)
+
+# Plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
+
+# Format plot.
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
