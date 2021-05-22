444c5f654ad41bb394ed9765d0eddb6680f0f731
flavienChamay <flavien.chamay@protonmail.com>
2021-05-22 12:13:21 +0200

    errors handling and multiple graphs functionality implemented

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -11,26 +11,35 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     # Get dates and high temperatures from the file.
-    highs, dates = [], []
+    highs, dates, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
